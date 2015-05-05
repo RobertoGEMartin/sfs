c06561db6c3df99ddf679ff6e119976faa36f96a
@rgem52 <rgomez@hi-iberia.es>
2015-05-05 09:39:54 +0200

    Preemptive error handling with fs.stat

--- static.js
+++ static.js
@@ -13,8 +13,26 @@ var server = http.createServer(function(req, res) {
     var path = join(root, url.pathname);
     var url = parse(req.url);
     var path = join(root, url.pathname);
-    var stream = fs.createReadStream(path);
-    stream.pipe(res);
+
+    fs.stat(path, function (err, stat) {
+        if (err) {
+            if ('ENOENT' == err.code) {
+                res.statusCode = 404;
+                res.end('Not Found');
+            } else {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            }
+        } else {
+            res.setHeader('Content-Length', stat.size);
+            var stream = fs.createReadStream(path);
+            stream.pipe(res);
+            stream.on('error', function (err) {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+        }
+    });
 });
 
 server.listen(3000);
